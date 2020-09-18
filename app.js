fc0d2c0e0e888442811cb0ba17085f880172664a
M.Noh <nohh.em@gmail.com>
2020-09-18 20:37:58 +0300

    fixed minor issue related to img path which caused creating multiple session

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 // );
 app.use((express.static(path.join(__dirname, 'public'))));
-app.use('/img', express.static(path.join(__dirname, 'images')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
 app.use(session({
   secret: 'my secret',
   store: store,

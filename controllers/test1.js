2ac2b8776b00800c07f2707c2cf2142ce04854b7
M.Noh <nohh.em@gmail.com>
2020-09-18 11:30:47 +0300

    problem with mock products insertion

---
+++ controllers/test1.js
@@ -0,0 +1,32 @@
+
+    ["Dress",
+    "Woman T-shirts",
+    "Woman Trousers",
+    "Blouse",
+    "Woman Shorts",
+    "Tights",
+    "Skirt",
+    "Woman Sweatshirt",
+    "Overalls",
+
+    "Woman Slipper",
+    "Sandals",
+    "Heeled shoes",
+    "Women Sneakers",
+    "Flat shoes",
+
+    "Men T-shirts",
+    "Men Shirt",
+    "Men Trousers",
+    "Shorts",
+    "Men Sweatshirt",
+    "Swim Shorts",
+
+    "Sports Shoes & Sneakers",
+    "Men Slipper",
+    "Classic Shoes",
+    "Casual Shoes",
+    "Girl Child",
+    "Boy",
+    "Baby Girl",
+    "Baby boy"]
